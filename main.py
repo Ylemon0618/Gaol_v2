3704184e6690c7e21088c3c672ddea6b7176f566
ylemon <kimkyuwan0618@gmail.com>
2024-12-28 20:36:48 +0900

    fix: Cog load, unload, reload 기능이 작동하지 않는 버그 수정

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
+OWNERS = list(map(int, os.environ.get('OWNERS').split()))
 intents = discord.Intents.all()
 
 
 class Bot(commands.Bot, ABC):
     def __init__(self):
-        super().__init__(intents=intents)
+        super().__init__(intents=intents, command_prefix="!admin ")
 
         self.remove_command("help")
 
@@ -39,7 +39,32 @@ class Bot(commands.Bot, ABC):
         else:
             raise error
 
-
 bot = Bot()
+
+
+@bot.command(name="load")
+async def load_(ctx: ApplicationContext, extension: str):
+    if ctx.author.id in OWNERS:
+        bot.load_extension(f"cogs.{extension}")
+        await ctx.send("Successfully loaded the Cog.")
+    else:
+        await ctx.send("레몬 전용임 ㅅㄱ")
+
+@bot.command(name="unload")
+async def unload_(ctx: ApplicationContext, extension: str):
+    if ctx.author.id in OWNERS:
+        bot.unload_extension(f"cogs.{extension}")
+        await ctx.send("Successfully unloaded the Cog.")
+    else:
+        await ctx.send("레몬 전용임 ㅅㄱ")
+
+@bot.command(name="reload")
+async def reload_(ctx: ApplicationContext, extension: str):
+    if ctx.author.id in OWNERS:
+        bot.reload_extension(f"cogs.{extension}")
+        await ctx.send("Successfully reloaded the Cog.")
+    else:
+        await ctx.send("레몬 전용임 ㅅㄱ")
+
 
 bot.run(os.environ.get('TOKEN'))
