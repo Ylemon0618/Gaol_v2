d43d121ce1ffed6a9a65d38364e1eb409068229d
ylemon <kimkyuwan0618@gmail.com>
2025-01-12 22:42:45 +0900

    refactor: convert, callback 함수 생성

--- modules/file_convert_button.py
+++ modules/file_convert_button.py
@@ -10,36 +10,42 @@ import os
 from modules.make_embed import makeEmbed, Color
 
 
+async def convert(file: discord.Attachment, ext: str):
+    directory = f"./modules/images/"
+    filename = file.filename
+
+    with open(directory + filename, "wb") as handler:
+        handler.write(requests.get(file.url).content)
+
+    new = Image.open(directory + filename).convert("RGBA")
+
+    os.remove(directory + filename)
+    filename = filename.replace(".webp", f".{ext}")
+    new.save(os.path.join(directory, filename))
+
+    return directory + filename
+
 class ConvertMainView(discord.ui.View):
     def __init__(self, file: discord.Attachment):
         super().__init__(timeout=None)
 
         self.file = file
 
-    async def convert_webp(self, ext: str, interaction: Interaction):
+    async def callback(self, button: discord.Button, interaction: Interaction):
         try:
-            directory = f"./modules/images/"
-            filename = self.file.filename
+            path = await convert(self.file, button.custom_id)
 
-            with open(directory + filename, "wb") as handler:
-                handler.write(requests.get(self.file.url).content)
+            await interaction.response.edit_message(file=discord.File(path), embed=None, view=None)
 
-            new = Image.open(directory + filename).convert("RGBA")
-
-            os.remove(directory + filename)
-            filename = filename.replace(".webp", f".{ext}")
-            new.save(os.path.join(directory, filename))
-
-            await interaction.response.edit_message(file=discord.File(directory + filename), embed=None, view=None)
-
-            os.remove(directory + filename)
+            os.remove(path)
         except Exception as e:
-            return await interaction.response.edit_message(embed=makeEmbed(":warning: Error :warning:", f"{e}", Color.error), view=None)
+            await interaction.response.edit_message(embed=makeEmbed(":warning: Error :warning:", f"{e}", Color.error),
+                                                    view=None)
 
     @discord.ui.button(label="png", custom_id="png", style=discord.ButtonStyle.blurple)
     async def png_button_callback(self, button: discord.Button, interaction: Interaction):
-        await self.convert_webp(button.custom_id, interaction)
+        await self.callback(button, interaction)
 
     @discord.ui.button(label="jpeg", custom_id="jpeg", style=discord.ButtonStyle.green)
     async def jpeg_button_callback(self, button: discord.Button, interaction: Interaction):
-        await self.convert_webp(button.custom_id, interaction)
+        await self.callback(button, interaction)
