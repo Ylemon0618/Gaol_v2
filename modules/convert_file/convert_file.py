7df87d0c9a79b528d9c434ee7e3aeadcfd3f7a18
ylemon <kimkyuwan0618@gmail.com>
2025-01-16 22:29:14 +0900

    build: 경로 수정

---
+++ modules/convert_file/convert_file.py
@@ -0,0 +1,21 @@
+import discord
+
+from PIL import Image
+import requests
+import os
+
+
+async def Convert(file: discord.Attachment, ext: str):
+    directory = "./images/"
+    filename = file.filename
+
+    with open(directory + filename, "wb") as handler:
+        handler.write(requests.get(file.url).content)
+
+    new = Image.open(directory + filename).convert("RGBA" if ext == "png" else "RGB")
+
+    os.remove(directory + filename)
+    filename = '.'.join(filename.split('.')[:-1]) + f".{ext}"
+    new.save(os.path.join(directory, filename))
+
+    return directory + filename
