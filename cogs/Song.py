b09b013bf16156002ba0683e095852287c3c0fe4
ylemon <kimkyuwan0618@gmail.com>
2025-01-18 00:47:59 +0900

    test: 서버의 downloads 폴더를 비우기 위한 명령어

--- cogs/Song.py
+++ cogs/Song.py
@@ -216,6 +216,12 @@ class Song(commands.Cog):
 
         await ctx.respond(embed=embed, view=QueueMainView(self.players[ctx.guild.id].queue, self.queue_listed, title))
 
+    @song_commands.command(name="test")
+    async def test_(self):
+        dir = "downloads"
+        for file in os.listdir(dir):
+            os.remove(os.path.join(dir, file))
+
 
 def setup(bot):
     bot.add_cog(Song(bot))
