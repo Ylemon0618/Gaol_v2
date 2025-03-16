4c22e5dc338cee2808bde20b218b4dc9097a6ba8
ylemon <kimkyuwan0618@gmail.com>
2025-03-16 14:54:35 +0900

    chore: Remove blanks

--- modules/messages/embeds.py
+++ modules/messages/embeds.py
@@ -41,102 +41,52 @@ class SongEmbed:
 
 class HelpEmbed:
     choose_item = makeEmbed(":speech_left: Help :speech_left:",
-                            """Do you need some help?
-                            Please select an item below.
-                            
-                            도움이 필요하신가요?
-                            아래에서 항목을 선택 해 주세요.""",
+                            "Do you need some help?\nPlease select an item below.\n\n도움이 필요하신가요?\n아래에서 항목을 선택 해 주세요.",
                             Color.success)
 
     choose_command = makeEmbed(":question: Command List | 명령어 도움말 :question:",
-                               """Please choose a group of commands you need help with.
-                               
-                               사용법이 궁금한 명령어를 아래에서 선택 해 주세요.""",
+                               "Please choose a group of commands you need help with.\n\n사용법이 궁금한 명령어를 아래에서 선택 해 주세요.",
                                Color.success)
 
     commands = {
         "song": {
             "ko": makeEmbed(":musical_note: Song | 노래 :musical_note:",
-                            """음악 재생과 관련된 명령어입니다.
-                            
-                            [yt-dlp](<https://github.com/yt-dlp/yt-dlp>) 라이브러리를 사용해 제작되었습니다.
-                            
-                            *이 명령어는 유튜브의 정책을 지키지 않을 수도 있습니다.*
-                            ㅤ
-                            ㅤ
-                            """,  # 줄 구분용 공백문자
+                            "음악 재생과 관련된 명령어입니다.\n[yt-dlp](<https://github.com/yt-dlp/yt-dlp>) 라이브러리를 사용해 제작되었습니다.\n\n*이 명령어는 유튜브의 정책을 지키지 않을 수도 있습니다.*\nㅤ\nㅤ",
                             Color.success),
             "en": makeEmbed(":musical_note: Song | 노래 :musical_note:",
-                            """Commands for playing music.
-                            
-                            These commands are created using a library: [yt-dlp](<https://github.com/yt-dlp/yt-dlp>).
-                            
-                            *These commands may not comply with YouTube's policy.*
-                            ㅤ
-                            ㅤ
-                            """,  # 줄 구분용 공백문자
+                            "Commands for playing music.\nThese commands are created using a library: [yt-dlp](<https://github.com/yt-dlp/yt-dlp>).\n\n*These commands may not comply with YouTube's policy.*\nㅤ\nㅤ",
                             Color.success)
         },
         "file": {
             "ko": makeEmbed(":file_folder: File | 파일 :file_folder:",
-                            """파일 관리를 위한 명령어입니다.
-                            ㅤ
-                            ㅤ
-                            """,  # 줄 구분용 공백문자
+                            "파일 관리를 위한 명령어입니다.\nㅤ\nㅤ",
                             Color.success),
             "en": makeEmbed(":file_folder: File | 파일 :file_folder:",
-                            """Commands for managing files.
-                            ㅤ
-                            ㅤ
-                            """,  # 줄 구분용 공백문자
+                            "Commands for managing files.\nㅤ\nㅤ",
                             Color.success)
         },
         "music": {
             "ko": makeEmbed(":musical_keyboard: Music | 음악 :musical_keyboard:",
-                            """음악과 관련된 명령어입니다.
-                            
-                            *이 명령어는 노래를 재생하는 명령어가 아닙니다.
-                            음악 재생은* `/노래` *를 이용해 주세요.*
-                            ㅤ
-                            ㅤ
-                            """,  # 줄 구분용 공백문자
+                            "음악과 관련된 명령어입니다.\n\n*이 명령어는 노래를 재생하는 명령어가 아닙니다.\n음악 재생은* `/노래` *를 이용해 주세요.*\nㅤ\nㅤ",  # 줄 구분용 공백문자
                             Color.success),
             "en": makeEmbed(":musical_keyboard: Music | 음악 :musical_keyboard:",
-                            """Commands for music.
-                            
-                            *These commands do not play songs.
-                            To play music, please use* `/Song` *instead.*
-                            ㅤ
-                            ㅤ
-                            """,  # 줄 구분용 공백문자
+                            "Commands for music.\n\n*These commands do not play songs.\nTo play music, please use* `/Song` *instead.*\nㅤ\nㅤ",  # 줄 구분용 공백문자
                             Color.success)
         },
         "game": {
             "ko": makeEmbed(":video_game: Game | 게임 :video_game:",
-                            """봇 또는 유저와 게임을 플레이 할 수 있는 명령어입니다.
-                            ㅤ
-                            ㅤ
-                            """,  # 줄 구분용 공백문자
+                            "봇 또는 유저와 게임을 플레이 할 수 있는 명령어입니다.\nㅤ\nㅤ",
                             Color.success),
             "en": makeEmbed(":video_game: Game | 게임 :video_game:",
-                            """Commands for playing game with the bot or user.
-                            ㅤ
-                            ㅤ
-                            """,  # 줄 구분용 공백문자
+                            "Commands for playing game with the bot or user.\nㅤ\nㅤ",
                             Color.success)
         },
         "utils": {
             "ko": makeEmbed(":grey_question: Utils | 유틸리티 :grey_question:",
-                            """각종 기능들을 사용할 수 있는 명령어입니다.
-                            ㅤ
-                            ㅤ
-                            """,  # 줄 구분용 공백문자
+                            "각종 기능들을 사용할 수 있는 명령어입니다.\nㅤ\nㅤ",
                             Color.success),
             "en": makeEmbed(":grey_question: Utils | 유틸리티 :grey_question:",
-                            """Commands of utility.
-                            ㅤ
-                            ㅤ
-                            """,  # 줄 구분용 공백문자
+                            "Commands of utility.\nㅤ\nㅤ",
                             Color.success)
         }
     }
