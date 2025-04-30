20c225fa93ce1d0d53bf0169b78c8bb6bf2d060a
Ylemon0618 <kimkyuwan0618@gmail.com>
2025-04-30 10:48:36 +0900

    feat: Connect coin toss to DB

--- modules/game_ui/coin_toss.py
+++ modules/game_ui/coin_toss.py
@@ -4,6 +4,7 @@ import discord
 from discord import ApplicationContext
 
 from modules.make_embed import makeEmbed, Color
+from modules.check_balance import *
 
 
 async def coin_toss(ctx: ApplicationContext):
@@ -19,6 +20,14 @@ class CoinTossBettingModal(discord.ui.Modal):
     async def callback(self, interaction: discord.Interaction):
         try:
             bet_amount = int(self.children[0].value)
+            balance = get_user_balance(interaction.user.id)
+
+            if bet_amount > balance:
+                return await interaction.response.send_message(embed=makeEmbed(":warning: Error :warning:",
+                                                                                 "잔액이 부족합니다.", Color.error),
+                                                               ephemeral=True)
+
+            update_user_balance(interaction.user.id, -bet_amount)
         except ValueError:
             return await interaction.response.send_message(embed=makeEmbed(":warning: Error :warning:",
                                                                              "베팅액은 숫자여야 합니다.", Color.error),
@@ -30,7 +39,7 @@ class CoinTossBettingModal(discord.ui.Modal):
                                                            ephemeral=True)
 
         embed = makeEmbed(":coin: Coin Toss | 동전 던지기 :coin:",
-                              f"Please bet on the heads or the tails\n앞면 혹은 뒷면에 베팅 해 주세요.",
+                              f"Please bet on the heads or the tails\n앞면 혹은 뒷면에 베팅 해 주세요.\n\n-# If you do not select anything, all of the amount you bet will disappear.\n-# 아무것도 선택하지 않으면 베팅액 전부가 사라집니다.",
                               Color.success)
         embed.add_field(name="Betting Amount | 베팅액", value=f"{bet_amount}$", inline=False)
         embed.add_field(name="Probability | 확률", value="- Heads | 앞면: 49.99%\n- Tails | 뒷면: 49.99%\n- Side | 옆면: 0.02%", inline=True)
@@ -79,10 +88,14 @@ class CoinTossSelect(discord.ui.Select):
         else:
             dividend = self.bet_amount // 2
 
+        new_balance = update_user_balance(interaction.user.id, dividend)
+
         embed = makeEmbed(":coin: Coin Toss | 동전 던지기 :coin:","", Color.success)
         embed.add_field(name="Choice | 선택", value=f"{choice.capitalize()} | {result_kr.get(choice)}", inline=True)
         embed.add_field(name="Result | 결과", value=f"{result.capitalize()} | {result_kr.get(result)}", inline=True)
-        embed.add_field(name="Bet Amount | 베팅액", value=f"{self.bet_amount}$", inline=False)
-        embed.add_field(name="Dividend | 배당금", value=f"{dividend}$", inline=False)
+        embed.add_field(name="", value="", inline=False)
+        embed.add_field(name="Bet Amount | 베팅액", value=f"{self.bet_amount}$", inline=True)
+        embed.add_field(name="Dividend | 배당금", value=f"{dividend}$", inline=True)
+        embed.add_field(name="Balance | 잔고", value=f"{new_balance}$", inline=False)
 
         await interaction.response.edit_message(embed=embed, view=None)
